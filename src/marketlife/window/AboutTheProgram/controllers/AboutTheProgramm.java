f30bab6dd94cb025f3bc18855238c49dae5d1f34
Viktor <coolezhik@yandex.ru>
2017-02-12 19:58:57 +0300

    Добавлены формы "О программе" и "Работа со словарями", но не реализован функционал работы с ними.

---
+++ src/marketlife/window/AboutTheProgram/controllers/AboutTheProgramm.java
@@ -0,0 +1,7 @@
+package marketlife.window.AboutTheProgram.controllers;
+
+/**
+ * Created by Viktor on 12.02.2017.
+ */
+public class AboutTheProgramm {
+}
---
+++ src/marketlife/window/WorkWithDictionaries/controllers/WorkWithDictionaries.java
@@ -0,0 +1,7 @@
+package marketlife.window.WorkWithDictionaries.controllers;
+
+/**
+ * Created by Viktor on 12.02.2017.
+ */
+public class WorkWithDictionaries {
+}
